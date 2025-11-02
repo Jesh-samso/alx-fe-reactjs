e6555191d68e449a50687758a83206d18c4ff296
Jesh-samso <samsonopondo1274@gmail.com>
2025-11-02 23:44:37 +0300

    component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,7 +1,7 @@
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
-import WelcomeMessage from './WelcomeMessage';
+import WelcomeMessage from './components/WelcomeMessage';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -14,7 +14,7 @@ function App() {
     
     <>
      <WelcomeMessage />  
-     <Header />   
+     <Header/>   
      <MainContent/> 
      <Footer />   
       <div>
