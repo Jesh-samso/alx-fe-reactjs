079cb10193f8838c91b997f978365977efdd7099
Jesh-samso <samsonopondo1274@gmail.com>
2025-11-03 21:15:50 +0300

    creating multiple components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -14,8 +14,8 @@ function App() {
     
     <>
      <WelcomeMessage />  
-     <Header/>   
-     <MainContent/> 
+     <Header />   
+     <MainContent /> 
      <Footer />   
       <div>
         <a href="https://vite.dev" target="_blank">
