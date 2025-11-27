e0e814db155fdde6251f2524a9fce4c33f5fc661
Jesh-samso <samsonopondo1274@gmail.com>
2025-11-28 01:00:11 +0300

    adding Navbar.jsx

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -6,12 +6,23 @@ import Home from './components/Home.jsx'
 import About from './components/About.jsx'
 import Services from './components/Services.jsx'
 import Contact from './components/Contact.jsx'
+import Navbar from './components/Navbar.jsx'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
     <Home />
     <About />
     <Services />
