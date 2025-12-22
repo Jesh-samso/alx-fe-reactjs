f2fc17675f6a6ef8f3e814ea50b3c475bc9f988c
Jesh-samso <samsonopondo1274@gmail.com>
2025-12-22 14:39:34 +0300

    adding image specifications to the userprofile component

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -4,9 +4,9 @@ function UserProfile() {
   return (
     <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
       <img
-        src="https://via.placeholder.com/150"
+        src="elon musk.jpg"
         alt="User"
-        className="rounded-full w-10 h-10 mx-auto"
+        className="rounded-full w-36 h-36 mx-auto"
       />
       <h1 className="text-xl text-blue-800 my-4 text-center">John Doe</h1>
       <p className="text-gray-600 text-base text-center">
