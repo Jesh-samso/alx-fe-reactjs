321d47da02f6b1f0baa1b6ddebf5f7e0816ee083
Jesh-samso <samsonopondo1274@gmail.com>
2025-12-23 12:33:12 +0300

    Build home page with recipe cards using Tailwind CSS

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -2,13 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import HomePage from "./components/HomePage";
 
 function App() {
-  return (
-    <div className="text-blue-500 text-2xl font-bold">
-      Hello Tailwind!
-    </div>
-  );
+  return <HomePage />;
 }
 
 export default App;
